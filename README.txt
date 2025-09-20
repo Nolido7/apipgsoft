c0036238920c7738befe3fe25487d008e40870db
Nolido7 <odilonmax08@gmail.com>
2025-09-20 04:04:45 -0300

    Primeiro commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+@galaxysevers - @rapha_galaxysevers
+
+En-us
+
+If you want me to install or continue the installation
+A fixed amount of $50 will be charged 
---
+++ includes/connect.php
@@ -0,0 +1,6 @@
+<?php
+		$link = mysqli_connect("localhost", "tigre", "LKmijbY8HBkGnBhn");
+		mysqli_select_db($link, "tigre");
+		mysqli_query($link, "SET CHARACTER SET utf8");
+		?>
+		
